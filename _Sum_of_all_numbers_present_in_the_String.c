b5fe0128fac754bece9fd0539707dd04b5dff1c4
Singh543214 <155215638+Singh543214@users.noreply.github.com>
2024-01-03 12:27:33 +0530

    #include<stdio.h> #include<string.h> int main() {     int max=0;     char ch[200];     scanf("%[^ ]s",ch);     for(int i=0;i<=strlen(ch);i++){         if(ch[i]>='0'&&ch[i]<='9'){             max+=(ch[i]-'0');         }     }     printf("%d",max); }

---
+++ _Sum_of_all_numbers_present_in_the_String.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int max=0;
+    char ch[200];
+    scanf("%[^
+]s",ch);
+    for(int i=0;i<=strlen(ch);i++){
+        if(ch[i]>='0'&&ch[i]<='9'){
+            max+=(ch[i]-'0');
+        }
+    }
+    printf("%d",max);
+}
